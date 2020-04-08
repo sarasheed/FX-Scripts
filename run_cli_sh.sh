5e339cfee268ea67ebd0392aef3e0db411304257
luqmanshareef <luqmanshareef@yahoo.com>
2020-04-08 10:44:21 +0530

    Run script fix

--- run_cli_sh.sh
+++ run_cli_sh.sh
@@ -1,4 +1,2 @@
-#APisec Command Line Tool
-
-wget  https://www.dropbox.com/s/j543w6edcuudeo0/CommandLine-0.0.1-SNAPSHOT.jar?dl=1  -O CommandLine.jar
+wget https://www.dropbox.com/s/j543w6edcuudeo0/CommandLine-0.0.1-SNAPSHOT.jar?dl=1 -O CommandLine.jar
 java -jar CommandLine.jar
