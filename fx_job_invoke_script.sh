35834a1418c526a7f90cf8032d2e7e040929b5d5
sarasheed <30456479+sarasheed@users.noreply.github.com>
2020-08-25 12:22:10 +0530

    Update fx_job_invoke_script.sh

--- fx_job_invoke_script.sh
+++ fx_job_invoke_script.sh
@@ -5,24 +5,17 @@ FX_USER=$1
 FX_PWD=$2
 FX_JOBID=$3
 REGION=$4
-TAGS=$5
-SUITES=$6
-CATEGORIES=$7
-
-
-echo "user=${FX_USER}" 
-echo "region=${REGION}"
-echo "jobid=${FX_JOBID}"
-
-
-runId=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" https://cloud.fxlabs.io/api/v1/runs/job/${FX_JOBID}?region=${REGION} | jq -r '.["data"]|.id')
+FX_ENVID=$5
+FX_PROJECTID=$6
+ 
+runId=$(curl --location --request --header -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" "https://cloud.fxlabs.io/api/v1/runs/job/${FX_JOBID}?region=${REGION}&env=${FX_ENVID}&projectId=${FX_PROJECTID}" | jq -r '.["data"]|.id')
 
 echo "runId =" $runId
 if [ -z "$runId" ]
 then
 	  echo "RunId = " "$runId"
           echo "Invalid runid"
-	  echo $(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" https://cloud.fxlabs.io/api/v1/runs/job/${FX_JOBID}?region=${REGION})
+	  echo $(curl --location --request --header -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" "https://cloud.fxlabs.io/api/v1/runs/job/${FX_JOBID}?region=${REGION}&env=${FX_ENVID}&projectId=${FX_PROJECTID}" | jq -r '.["data"]|.id')
           exit 1
 fi
 
@@ -37,24 +30,20 @@ while [ "$taskStatus" == "WAITING" -o "$taskStatus" == "PROCESSING" ]
 		sleep 5
 		 echo "Checking Status...."
 
-		passPercent=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}" https://cloud.fxlabs.io/api/v1/runs/${runId} | jq -r '.["data"]|.ciCdStatus')
+		passPercent=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}" https://cloud.fxlabs.io/api/v1/runs/${runId} | jq -r '.["data"]|.ciCdStatus') 
                         
 			IFS=':' read -r -a array <<< "$passPercent"
 			
 			taskStatus="${array[0]}"			
 
-			echo "Status =" "${array[0]}" " Success Percent =" "${array[1]}"  " Total Tests =" "${array[2]}" " Time Taken =" "${array[4]}" " Run =" "${array[5]}"
+			echo "Status =" "${array[0]}" " Success Percent =" "${array[1]}"  " Total Tests =" "${array[2]}" " Total Failed =" "${array[3]}" " Run =" "${array[6]}"
 			
 				
 
 		if [ "$taskStatus" == "COMPLETED" ];then
-
             echo "------------------------------------------------"
-			echo  "Run detail link https://cloud.fxlabs.io"${array[6]}
-			echo "------------------------------------------------"
-			echo  "${array[7]}"
-			echo "------------------------------------------------"
-                        
+			echo  "Run detail link https://cloud.fxlabs.io${array[7]}"			
+			echo "-----------------------------------------------"
                 	echo "Job run successfully completed"
                         exit 0
 
@@ -66,11 +55,10 @@ echo "Task Status = " $taskStatus
  exit 1
 fi
 
-echo $(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}" https://cloud.fxlabs.io/api/v1/runs/${runId})
+echo "$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}" https://cloud.fxlabs.io/api/v1/runs/${runId})"
 exit 1
 
 return 0
-
 
 
 
